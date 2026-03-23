9836ce92bc52bcca2b9821455e54f5753e8b9333
emirald2202 <anjali.ayurclinic@gmail.com>
2026-03-23 13:20:42 +0530

    url location chanaged

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import API_BASE_URL from "../config/api.config";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: API_BASE_URL
 });
 
 instance.interceptors.request.use((config) => {
---
+++ frontend/src/config/api.config.js
@@ -0,0 +1,5 @@
+// API base URL configuration
+// Change this value when deploying to production
+const API_BASE_URL = "http://localhost:5000/api";
+
+export default API_BASE_URL;
